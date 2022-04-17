14439d6eb7d14f6cdf32d0e64acb3187b1da8f09
Lucy Swett <lucy.swett@alum.lehigh.edu>
2022-04-16 20:58:31 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
+import { useState } from 'react';
+import Calendar from 'react-calendar';
+import 'react-calendar/dist/Calendar.css';
 import './App.css';
 
 function App() {
+  const [date, setDate] = useState(new Date());
+  let arr = []
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='app'>
+      <div className='container'>
+        <div className='row justify-content-center'>
+          <div className='col-lg-6'>
+      <h1 className='text-center mt-5 mb-4'>React Calendar</h1>
+      <div className='calendar-container d-flex justify-content-center'>
+      <Calendar 
+        onChange={setDate} 
+        value={date} 
+        onClickDay={
+          // (day, value, event) => {
+          //   console.log("date", date)
+          //   arr.map(date => new Date(date))
+          //   console.log(day, value.target, this)
+          //   console.log(arr)
+          // }
+          ({ activeStartDate, date, view }) => view === 'month' && date.getDay() === 3 ? 'wednesday' : console.log('not wed', activeStartDate)
+
+        } 
+        // titleClassName={
+        // }
+        />
+      </div>
+      <p className='text-center my-4'>
+        <span className='bold'>Selected Date:</span>{' '}
+        {date.toDateString()}
+      </p>
+      </div>
+      </div>
+    </div>
     </div>
   );
 }
