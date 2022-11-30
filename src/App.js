e348cc20c4e9832c57f0249e96ea039441d9fe98
Lucy Swett <lucy.swett@alum.lehigh.edu>
2022-11-30 14:15:48 -0500

    clean up code

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import Calendar from 'react-calendar';
 import 'react-calendar/dist/Calendar.css';
 import './App.css';
@@ -6,20 +6,25 @@ import './App.css';
 function App() {
   const [date, setDate] = useState(new Date());
   const [contactDays, setContactDays] = useState([])
+  // const savedDays = JSON.parse(localStorage.getItem("contactDays"))
+
+  // useEffect(() => {
+  //   localStorage.setItem("contactDays", JSON.stringify(contactDays))
+  // })
 
   return (
     <div className='app'>
       <div className='container'>
         <div className='row justify-content-center'>
           <div className='col-lg-6'>
-            <h1 className='text-center mt-5 mb-4'>React Calendar</h1>
+            <h1 className='text-center mt-5 mb-4'>Contact Calendar</h1>
             <div className='calendar-container d-flex justify-content-center'>
               <Calendar 
+                calendarType='US'
                 onChange={setDate} 
                 value={date} 
-                onClickDay={ date => setContactDays([...contactDays, date.toDateString()])
-                } 
-                // titleClassName={}
+                onClickDay={ date => contactDays.includes(date.toDateString()) ? setContactDays(contactDays.filter(day => day !== date.toDateString())) : setContactDays([...contactDays, date.toDateString()]) } 
+                tileClassName={({ date, view }) => view === 'month' && contactDays.includes(date.toDateString()) ? 'selected' : null}
               />
             </div>
             <p className='text-center my-4'>
